0c323fada33867ac4e3eae119efc1c680107475d
Filip Kalus <kalus.filip25@gmail.com>
2023-01-19 20:33:55 +0100

    DB login/register funkcionalita

--- commands.js
+++ commands.js
@@ -1,4 +1,14 @@
 import garbageitems from "./items/garbageitems.js";
+import dotenv from 'dotenv'
+dotenv.config()
+
+// const MongoClient = require('mongodb').MongoClient;
+import { MongoClient } from 'mongodb'
+// const MongoClient = new Mongo.MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@typerdb.ztj7z.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+// onst bcrypt = require('bcrypt');
+import bcrypt from 'bcrypt'
 
 export function checkCommand(user, s, socket) {
     let array = s.split(" ");
@@ -15,6 +25,7 @@ export function checkCommand(user, s, socket) {
 }
 
 var commands = {
+    // TODO: Specifikuj ci sa ma command vypisat iba pre uzivatela alebo pre vsetkych
     duel: function(hrac1, hrac2) {
         // DB THINGS
         return `Hrac ${hrac1} zautocil na hraca ${hrac2}`
@@ -45,9 +56,40 @@ var commands = {
         return "";
     },
     login: function(username, password) {
-        return "";
+        let result = ""
+        client.connect(async err => {
+          const collection = client.db("typerdb").collection("users");
+          // perform actions on the collection object
+          const user = await collection.findOne({ username });
+          if (!user) {
+              console.log("Neexistujes")
+              return "Neexistujes";
+          }
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (!isMatch) {
+              console.log("Zle heslo")
+              return "Zle heslo";
+          } else {
+              // generate a JWT token and send it to the client
+              // const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+              console.log("Prihlaseny")
+              result = "Prihlaseny"
+  }
+          client.close();
+        });
+        return result;
     },
     register: function(username, password, passwordRepeat) {
-        return "";
+        // TODO: Add password and password repeat validation, 
+        let result;
+        client.connect(async err => {
+          const collection = client.db("typerdb").collection("users");
+          // perform actions on the collection object
+          const hashedPassword = await bcrypt.hash(password, 10);
+          const newUser = { username, password: hashedPassword };
+          result = await collection.insertOne(newUser);
+          client.close();
+        });
+        return result;
     }
 }
